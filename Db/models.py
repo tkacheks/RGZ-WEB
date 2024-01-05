8b6fa1b4ef5e9128e6f57a36573ffe9c4f44151b
karina <tkacheks03@mail.ru>
2024-01-06 04:43:31 +0700

    Авторизация и аутентификация

--- Db/models.py
+++ Db/models.py
@@ -1,4 +1,5 @@
 from . import db
+from sqlalchemy import func
 
 
 class users(db.Model):
@@ -12,5 +13,5 @@ class initiatives(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title = db.Column(db.String(50), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    creation_date = db.Column(DateTime(timezone=True), server_default=func.now())
-    rating = db.Column()
+    creation_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    rating = db.Column(db.Integer, default=0)
--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, session, render_template, redirect, request, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
 from Db import db
-from Db.models import
+from Db.models import users, initiatives
 
 
 app = Flask(__name__)
@@ -20,3 +19,99 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{hos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+
+@app.route('/')
+def start():
+    return redirect(url_for('main'))
+
+
+@app.route("/app/index", methods=['GET', 'POST'])
+def main():
+    if 'username' not in session:
+        return redirect(url_for('registerPage'))
+
+    all_users = users.query.all()
+
+    visible_user = session.get('username', 'Anon')
+
+    return render_template('index.html', name=visible_user, all_users=all_users)
+
+
+@app.route('/app/register', methods=['GET', 'POST'])
+def registerPage():
+    errors = []
+
+    if request.method == 'GET':
+        return render_template("register.html", errors=errors)
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not (username or password):
+        errors.append("Пожалуйста, заполните все поля")
+        print(errors)
+        return render_template("register.html", errors=errors)
+   
+    existing_user = users.query.filter_by(username=username).first()
+
+    if existing_user:
+        errors.append('Пользователь с данным именем уже существует')
+        return render_template('register.html', errors=errors, resultСur=existing_user)
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = users(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/app/login")
+
+
+@app.route('/app/login', methods=["GET", "POST"])
+def loginPage():
+    errors = []
+
+    if request.method == 'GET':
+        return render_template("login.html", errors=errors)
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not (username or password):
+        errors.append("Пожалуйста, заполните все поля")
+        return render_template("login.html", errors=errors)
+
+    user = users.query.filter_by(username=username).first()
+
+    if user is None or not check_password_hash(user.password, password):
+        errors.append('Неправильный пользователь или пароль')
+        return render_template("login.html", errors=errors)
+
+    session['id'] = user.id
+    session['username'] = user.username
+
+    return redirect("/app/index")
+
+
+@app.route("/app/new_initiative", methods=["GET", "POST"])
+def createInitiative():
+    errors = []
+    username = session.get('username')
+
+    if username == 'admin':
+        if request.method == 'POST':
+            title = request.form.get('title')
+            text = request.form.get('text')
+
+            if not (title and text):
+                errors.append('Пожалуйста, заполните все поля')
+                return render_template('new_initiative.html', errors=errors)
+
+            new_initiative = initiatives(title=title, text=text)
+            db.session.add(new_initiative)
+            db.session.commit()
+
+            return redirect(url_for('all_initiatives'))
+
+    return render_template('new_initiative.html', errors=errors)
